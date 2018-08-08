4056fdca6bb1c6ce1d253a39a453342b84ac8aeb
Vincent Wei <kernelbean@gmail.com>
2018-08-08 12:11:14 -0700

    upgrade docker version

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -2,9 +2,9 @@
 # Copied from https://github.com/aws/aws-codebuild-docker-images/blob/master/ubuntu/docker/1.12.1/dockerd-entrypoint.sh
 set -e
 
-/usr/local/bin/dind docker daemon \
+/usr/local/bin/dockerd \
 	--host=unix:///var/run/docker.sock \
-	--host=tcp://0.0.0.0:2375 \
+	--host=tcp://127.0.0.1:2375 \
 	--storage-driver=overlay &>/var/log/docker.log &
 
 
